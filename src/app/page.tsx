0a911698219aa56e9c28ad26c698107a957fe331
Jun <rodrigo.suganob@sparcmarina.com>
2025-05-07 21:06:49 +0800

    FEAT: Setting app write

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+
+
 export default function Home() {
+ 
+
   return (
     <div>
       <Input />
