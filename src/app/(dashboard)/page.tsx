b25d39e69f28f87c41e8cd7a16463bee0c1e7fa5
Jun <rodrigo.suganob@sparcmarina.com>
2025-05-10 18:35:18 +0800

    REF: Server Queries

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
-import { getCurrent } from "@/features/auth/action";
+import { getCurrent } from "@/features/auth/queries";
 
 import { redirect } from "next/navigation";
-import { getWorkspaces } from "@/features/workspaces/action";
+import { getWorkspaces } from "@/features/workspaces/queries";
 
 export default async function Home() {
   const user = await getCurrent();
@@ -15,5 +15,5 @@ export default async function Home() {
     redirect(`/workspaces/${workspaces.documents[0].$id}`);
   }
 
-  
+
 };
