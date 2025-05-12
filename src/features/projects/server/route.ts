c63838bd155ce5550a1c5c5c1f11fe7f5b934844
Jun <rodrigo.suganob@sparcmarina.com>
2025-05-12 11:08:18 +0800

    FEAT: Add Create projects function at routes

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,20 +1,51 @@
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, WORKSPACES_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { z } from "zod";
-// import { createProjectSchema } from "../"
+import { creatProjectSchema } from "../schema"
+import { generateInviteCode } from "@/lib/utils";
+import { MemberRole } from "@/features/members/types";
 
 
 const app = new Hono()
 
-  //   .post(
-  //   "/",
-  //   sessionMiddleware,
-  //   zValidator("form", createProjectSchema),
-  // )
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("form", creatProjectSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const user = c.get("user");
+      const { name, workspaceId } = c.req.valid("form")
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" })
+      }
+
+
+      const project = await databases.createDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        ID.unique(),
+        {
+          name,
+          userId: user.$id,
+          workspaceId,
+        },
+      );
+
+      return c.json({ data: project });
+    }
+  )
 
   .get(
     "/",
